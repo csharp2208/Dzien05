4f7f412b8a34baf38efd6c0700d102f3e41f4b77
kurs <kurs@KURS-KOMPUTER>
2020-09-12 16:57:16 +0200

    update

--- StooqParser/Form1.Designer.cs
+++ StooqParser/Form1.Designer.cs
@@ -28,12 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.tbTicker = new System.Windows.Forms.TextBox();
             this.dtStart = new System.Windows.Forms.DateTimePicker();
             this.dtStop = new System.Windows.Forms.DateTimePicker();
             this.btnParse = new System.Windows.Forms.Button();
             this.dgvData = new System.Windows.Forms.DataGridView();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
             ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
             this.SuspendLayout();
             // 
             // tbTicker
@@ -75,16 +80,36 @@
             this.dgvData.AllowUserToDeleteRows = false;
             this.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvData.Location = new System.Drawing.Point(24, 68);
+            this.dgvData.MultiSelect = false;
             this.dgvData.Name = "dgvData";
             this.dgvData.ReadOnly = true;
-            this.dgvData.Size = new System.Drawing.Size(764, 370);
+            this.dgvData.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvData.Size = new System.Drawing.Size(615, 485);
             this.dgvData.TabIndex = 4;
+            // 
+            // chart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart.Legends.Add(legend1);
+            this.chart.Location = new System.Drawing.Point(676, 68);
+            this.chart.Name = "chart";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "series";
+            this.chart.Series.Add(series1);
+            this.chart.Size = new System.Drawing.Size(459, 485);
+            this.chart.TabIndex = 5;
+            this.chart.Text = "chart1";
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1147, 565);
+            this.Controls.Add(this.chart);
             this.Controls.Add(this.dgvData);
             this.Controls.Add(this.btnParse);
             this.Controls.Add(this.dtStop);
@@ -95,6 +120,7 @@
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.FormMain_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -107,6 +133,7 @@
         private System.Windows.Forms.DateTimePicker dtStop;
         private System.Windows.Forms.Button btnParse;
         private System.Windows.Forms.DataGridView dgvData;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
     }
 }
 
--- StooqParser/Form1.cs
+++ StooqParser/Form1.cs
@@ -22,10 +22,23 @@ namespace StooqParser
         {
             dtStop.Value = DateTime.Now;
             dtStart.Value = DateTime.Now.AddMonths(-1);
+
+            dgvData.Columns.Clear();
+            dgvData.Columns.Add("DATE", "Data notowania");
+            dgvData.Columns.Add("CLOSE", "Kurs zamknięcia");
+            dgvData.Columns.Add("VOLUME", "Wolumen akcji");
+
+            dgvData.Columns[0].Width = 200;
+            dgvData.Columns[1].Width = 300;
+            dgvData.Columns[2].Width = 350;
+
         }
 
         private void btnParse_Click(object sender, EventArgs e)
         {
+            dgvData.Rows.Clear(); //usuwa stare wiersze
+            chart.Series[0].Points.Clear();
+
             // https://stooq.pl/q/d/l/?s=cdr&d1=20191126&d2=20200911&i=d
             String ticker = tbTicker.Text.ToLower();
             String d1 = dtStart.Value.ToString("yyyyMMdd");
@@ -35,6 +48,16 @@ namespace StooqParser
             {
                 WebClient webClient = new WebClient();
                 String s = webClient.DownloadString(url);
+                string[] lines = s.Split('\n');
+                for (int i = 1; i < lines.Length; i++)
+                {
+                    string[] items = lines[i].Trim().Split(',');
+                    if (items.Length == 6)
+                    {
+                        dgvData.Rows.Add(new string[] { items[0], items[4], items[5] });
+                        chart.Series[0].Points.AddXY(i, Convert.ToDouble(items[4].Replace(".", ",")));
+                    }
+                }
             } catch (Exception exc)
             {
                 MessageBox.Show(exc.Message);
